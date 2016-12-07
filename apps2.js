276461b269ecb5aa962b8b007e4152097e32d45a
0peron <phubbard89@gmail.com>
2016-12-06 21:24:27 -0500

    working on keeping state of checked

--- apps2.js
+++ apps2.js
@@ -1,59 +1,100 @@
 var state = {
-    items: []
-}
-
-function addItem(state, item) {
-    state.items.push(item, {
-        checkedOff: false
-    });
-}
-
-function removeItem(state, item) {
-    var itemArray = state.items;
-    var index = {
-        for (var i = 0; i < intemsArray.length; i++) {
-            if (itemArray[i].name === item) {
-                index = i;
+    items: [
+        {
+            name: "apples",
+            checked: false
+            },
+        {
+            name: "oranges",
+            checked: false
+            },
+        {
+            name: "milk",
+            checked: false
+            },
+        {
+            name: "bread",
+            checked: true
             }
+        ]
+}
+
+function addItem(state, itemObj) {
+    state.items.push(itemObj);
+}
+
+function removeItem(state, itemName) {
+    var itemsArray = state.items;
+    var index;
+    for (var i = 0; i < itemsArray.length; i++) {
+        if (itemsArray[i].name === itemName) {
+            index = i;
         }
     }
-    itemArray.splice(index, 1);
+    itemsArray.splice(index, 1);
 }
-function renderList(state, element) {
-    var renderHTML = state.items.map(function (item) {
-        return (
-            '<li>' +
-            '<span class="shopping-item js-shopping-item">' + item + '</span>' +
-            '<div class="shopping-item-controls">' +
-            '<button class="js-shopping-item-toggle">' +
-            '<span class="button-label">check</span>' +
-            '</button>' +
-            '<button class="js-shopping-item-delete">' +
-            '<span class="button-label">delete</span>' +
-            '</button>' +
-            '</div>' +
-            '</li>'
-        );
-    })
-    element.html(renderHTML);
-    return renderList;
+
+function checkItem(state, itemName) {
+    var itemsArray = state.items;
+    for (var i = 0; i < state.items.length; i++) {
+        if (state.items[i].name === itemName) {
+            state.items[i].checked = true;
+        }
+    }
+}
+
+function renderList(state, JQueryElement) {
+    var renderedHTML = state.items.map(function (item) {
+        console.log(item);
+
+        var row = '';
+        row += '<li>';
+        if (item.checked == false) {
+            row += '<span class="shopping-item">' + item.name + '</span>';
+        } else {
+            row += '<span class="shopping-item shopping-item__checked">' + item.name + '</span>';
+        }
+        row += '<div class="shopping-item-controls">';
+        row += '<button class="shopping-item-toggle">';
+        row += '<span class="button-label">check</span>';
+        row += '</button>';
+        row += '<button class="shopping-item-delete">';
+        row += '<span class="button-label">delete</span>';
+        row += '</button>';
+        row += '</div>';
+        row += '</li>';
+
+        return row;
+    });
+    JQueryElement.html(renderedHTML);
 }
 
 $(document).ready(function () {
-    $('#js-shopping-list-form').submit(function (event) {
-        event.preventDefault();
-        var item = $('#shopping-list-entry').val();
-        addItem(state, item);
-        renderList(state, $('.shopping-list'));
-        this.reset();
-    });
 
-    $('<li>').click('button.shipping-item-toggle', function (event) {
-        $(this).closest('<li>').find('.shopping-item').toggleClass('.shopping-item__checked');
+    renderList(state, $('.shopping-list'));
+    $('#js-shopping-list-form').on('submit keypress', function (event) {
+        if (event.type === 'keypress' && event.which === 13 || event.type === 'submit') {
+            event.preventDefault();
+            var itemName = $('#shopping-list-entry').val();
+            var shoppingItem = {
+                name: itemName,
+                checked: false
+            }
+            if (itemName) {
+                addItem(state, shoppingItem);
+                renderList(state, $('.shopping-list'));
+            }
+        }
     });
-    $('<li>').click('buttion.shopping-item-delete', function (event) {
-        var item = $(this).closest('<li>').find('.shopping-item').text();
-        removeItem(state, item);
-        renderList(state, $('.shopping-list'));
-    });
+});
+
+$('ul').on('click', 'button.shopping-item-toggle', function (event) {
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    checkItem(state, itemName);
+});
+
+$('ul').on('click', 'button.shopping-item-delete', function (event) {
+    var itemName = $(this).closest('li').find('.shopping-item').text();
+    removeItem(state, itemName);
+    renderList(state, $('.shopping-list'));
 });
